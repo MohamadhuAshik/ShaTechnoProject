6ad98ba56c4ddce83e69f391baa894f14817c3d0
MohamadhuAshik <mohamadashik268@gmail.com>
2024-11-02 22:44:48 +0530

    DB Production Change

--- src/database/database.config.js
+++ src/database/database.config.js
@@ -5,16 +5,17 @@ dotenv.config();
 
 const config = {
     server: '97.74.83.13',
-    // database: 'prod-shatechnosolutions',
+    database: 'prod-shatechnosolutions',
     // user: 'shatechno-proddev',
     // password: 'gXM5KMeAx',
 
     // database: 'dev.shatechno',
-    // user: 'dev.shatechno', 
-    // password: '7!jYo787n',
-    database: 'dev.shatechno',
     user: 'dev.shatechno',
     password: '7!jYo787n',
+
+    // database: 'dev.shatechno',
+    // user: 'dev.shatechno',
+    // password: '7!jYo787n',
     options: {
         trustedConnection: false,
         enableArithAbort: true,
